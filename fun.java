ec65d130745213b1d82f34dbbc54cd2e3e041dfc
Abhishek Mishra <abhishekbnm79@gmail.com>
2026-02-22 23:18:25 +0530

    Create fun.java

---
+++ fun.java
@@ -0,0 +1,13 @@
+public class fun {
+   int show(int a,int b){
+     int c=a+b;  
+    return c;
+    }
+    public static void main(String[] args) {
+        fun ob=new fun();
+       // ob.show(5, 5);
+      
+        System.out.println( ob.show(5, 5));
+       
+    }
+}
